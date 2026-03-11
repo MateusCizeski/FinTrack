c3e73b7c18d560c03cc135f62537f44dcb8f3279
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2026-03-11 07:23:59 -0300

    fix for the project

--- FinTrack/App.xaml.cs
+++ FinTrack/App.xaml.cs
@@ -1,23 +1,13 @@
-﻿namespace FinTrack
+﻿using FinTrack.Views;
+
+namespace FinTrack
 {
     public partial class App : Application
     {
-        public App()
+        public App(TransactionList transactionList)
         {
-            Resources["AppBg"] = Color.FromArgb("#0B0D11");
-            Resources["Surface"] = Color.FromArgb("#13161D");
-            Resources["Surface2"] = Color.FromArgb("#1A1E28");
-            Resources["BorderColor"] = Color.FromArgb("#16FFFFFF");
-            Resources["Green"] = Color.FromArgb("#00E5A0");
-            Resources["GreenDim"] = Color.FromArgb("#1E00E5A0");
-            Resources["Coral"] = Color.FromArgb("#FF5C6A");
-            Resources["CoralDim"] = Color.FromArgb("#1EFF5C6A");
-            Resources["Gold"] = Color.FromArgb("#F0C040");
-            Resources["TextPrimary"] = Color.FromArgb("#E8EAF0");
-            Resources["TextMuted"] = Color.FromArgb("#5A6070");
-
             InitializeComponent();
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(transactionList);
         }
     }
 }
